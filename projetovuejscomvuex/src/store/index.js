599bf9807fbcbd68958e3291e9d48387bb1cadcb
thiago <thiagoe99@hotmail.com>
2022-08-23 15:46:00 -0300

    primeiros dados inseridos no vuex

--- projetovuejscomvuex/src/store/index.js
+++ projetovuejscomvuex/src/store/index.js
@@ -1,14 +1,34 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // é o estado no VueX, onde estao os dados, é como se fosse o data() da componente
   state: {
+    usuario: {
+      primeiro_nome: 'Joao',
+      ultimo_nome: 'Silva',
+      email: 'joao@silva.com',
+    }
   },
   getters: {
   },
+  // dentro do mutations que o estado é alterado, ela que possui essa responsabilidade
+  // na mutation sao funcoes sincronas, assim o programa so continua depois que ele finaliza uma funcao 
   mutations: {
+    // o state recebe o estado inteiro atual
+    salvarPrimeiroNome(state, payload){
+      state.usuario.primeiro_nome = payload;
+    }
   },
+  // da acoes dentro de um determinado contexto, ela utiliza a mutation para alterar o estado
+  // na action as funcoes sao assincronas
   actions: {
+    // o context é usado em casos que se deseja puxar dados do state 
+    salvarPrimeiroNome(context, payload){
+      // se usa commit pelo fato de que agora vamos comitar as mudancas
+      context.commit('salvarPrimeiroNome', payload)
+    }
   },
+  // é possivel dividir em modulos para melhor organizar 
   modules: {
   }
 })
--- projetovuejscomvuex/src/views/HomeView.vue
+++ projetovuejscomvuex/src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- obtendo dado direto do VueX -->
+    {{ $store.state.usuario.primeiro_nome}} <br>
+    <label for="">Primeiro nome</label> <br>
+    <input type="text" v-model="meuNome">
+    <button @click.prevent.stop="salvarNome">Salvar</button>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      meuNome: "",
+    }
+  },
+  methods: {
+    salvarNome(){
+      // para acessar as actions dentro do vuex utiliza o metodo dispatch
+      // this.$store.dispatch('salvarPrimeiroNome', this.meuNome);
+
+      // para alguns casos nao é nescessario chamar o action para depois chamar o muttation
+      // para acessar os mutations dentro do vuex utiliza o metodo commit
+      // this.$store.commit('salvarPrimeiroNome', this.meuNome);
+
+    }
   }
 }
 </script>
